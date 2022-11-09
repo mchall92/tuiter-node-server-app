31ce45801fdfa53e6a64e7d327767b3b496636cf
Martin Chien <martinchien92@gmail.com>
2022-11-09 16:32:07 +0800

    fixed predifined tuit

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -3,15 +3,15 @@ let tuits = posts;
 
 const createTuit = (req, res) => {
     const newTuit = req.body;
-    newTuit._id = (new Date()).getTime() + '';
     newTuit.likes = 0;
     newTuit.liked = false;
     tuits.push(newTuit);
     res.json(newTuit);
 }
 
-const findTuits = (req, res) =>
+const findTuits = (req, res) => {
     res.json(tuits);
+}
 
 const updateTuit = (req, res) => {
     const tuitdIdToUpdate = parseInt(req.params.tid);
