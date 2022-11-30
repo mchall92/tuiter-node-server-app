c1c3eb9c6d6ab9d5c55826f3aba055e1a09f8b60
Martin Chien <martinchien92@gmail.com>
2022-12-01 06:11:56 +0800

    added mongoose

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ mongoose.connect(CONNECTION_STRING);
 app.use(express.json());
 app.use(
     cors({
+        credentials: true,
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
         allowedHeaders: ['Content-Type', 'Authorization'],
         origin: '*',
