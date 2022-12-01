2cc7331b649748c7b0234255e0fa2934c4136c25
Martin Chien <martinchien92@gmail.com>
2022-12-02 00:03:34 +0800

    fixed cors

--- app.js
+++ app.js
@@ -14,14 +14,6 @@ mongoose.connect(CONNECTION_STRING);
 app.use(express.json());
 app.use(cors({ credentials: true, origin: true }))
 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
-
 HelloController(app)
 UserController(app)
 TuitsController(app)
